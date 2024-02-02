9e3d99202624dcb1e9bbcbfc6394fcaf46aa83d7
cathywalker <48182560+cathywalker@users.noreply.github.com>
2024-02-02 13:51:13 +0000

    text change

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -4,7 +4,7 @@ const Courses = () => {
   return (
     <div>
       <h1>Courses</h1>
-      <div>I am almost finished creating 2 courses to host on the likes of Udemy. One for translation of Angular to React and one for the opposite, React to Angular. Having studied and created projects personally and professionaly in both I felt I was perfectly situated to develop them, and hopefully other Devs can get value from them. They will be live in late 2023.
+      <div>I am almost finished creating 2 courses to host on the likes of Udemy. One for translation of Angular to React and one for the opposite, React to Angular. Having studied and created projects personally and professionaly in both I felt I was perfectly situated to develop them, and hopefully other Devs can get value from them. They will be live later in 2024.
         <h2>Topics include:</h2>
         
           <p>- Inputs to props</p>
