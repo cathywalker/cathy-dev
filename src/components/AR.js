c022ff16a55802caee5ee036c5c6905a05756b4d
cathywalker <48182560+cathywalker@users.noreply.github.com>
2024-11-08 14:57:56 +0000

    Add VP prototype text
    Vision Pro text.
    18 years

--- src/components/AR.js
+++ src/components/AR.js
@@ -4,7 +4,7 @@ const AR = () => {
   return (
     <div>
       <h1>Augmented Reality and Spatial Computing</h1>
-      <div>Most recently I have made a prototype for VisionOS. An App called Postcard Memories. When the user looks at a postcard or Fridge Magnet of a previous holiday they will be able to see a video from that holiday played out in their home in 3D space.</div>
+      <div>I am currently developing two prototypes for the Vision Pro, using Xcode, Reality Composer Pro, Swift, and SwiftUI. One of which I plan to deploy to the Apple Store early next year.</div>
       <div><p>I previously made an AR App for Botanic Gardens in Belfast. It recognises various landmarks at the park such as the statue of Kelvin, Sculptures and the Bandstands and displays information about them beside the landmark and some of them also trigger audio.</p></div>
       <img
           src="https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-700/botanic_LiZ8mGTkU.jpg"
